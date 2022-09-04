15e90e31265e4f054460416c9b41694d8d7f80e3
Mark Waite <mark.earl.waite@gmail.com>
2022-09-04 16:12:07 -0600

    Remove the skip tests experiment

--- test/groovy/mock/CurrentBuild.groovy
+++ test/groovy/mock/CurrentBuild.groovy
@@ -1,18 +1,11 @@
 package mock
 
-import net.sf.json.JSONObject
-
 /**
  * Mock currentBuild
  */
 class CurrentBuild implements Serializable {
-  String result = 'SUCCESS'
-  int number = 1
-  def changeSets = null
+  String result
   ArrayList buildCauses
-
-  public CurrentBuild() {
-  }
 
   public CurrentBuild(String result) {
     this.result = result
@@ -22,24 +15,5 @@ class CurrentBuild implements Serializable {
   public CurrentBuild(String result, ArrayList buildCauses) {
     this.result = result
     this.buildCauses = buildCauses
-  }
-
-  public CurrentBuild(int number) {
-    this.number = number
-    this.buildCauses = []
-  }
-
-  public CurrentBuild(int number, boolean initializeChangeSets) {
-    this.number = number
-    this.buildCauses = []
-    if (initializeChangeSets) {
-      List<String> fileOne = [ 'README.md', ]
-      def changeSetOne = [ commitId: 'SHA-1.1', author: 'writer.1', affectedFiles: fileOne ]
-      this.changeSets = [ [items:changeSetOne], ]
-    }
-  }
-
-  public JSONObject getBuildCauses() {
-    return new JSONObject()
   }
 }
