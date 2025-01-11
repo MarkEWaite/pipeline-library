e1ab7eed9620bea78bbf3b20a0ca243f02be64be
Mark Waite <mark.earl.waite@gmail.com>
2025-01-10 18:47:23 -0700

    Try setting core.autocrlf

--- vars/infra.groovy
+++ vars/infra.groovy
@@ -141,7 +141,11 @@ Object withFileShareServicePrincipal(Map options, Closure body) {
 Object checkoutSCM(String repo = null) {
   // Enable long paths to avoid problems with tests on Windows agents
   if (!isUnix()) {
-    bat 'git config --global core.eol lf && git config --global core.longpaths true && git config --global --list'
+    bat '''
+        git config --global core.autocrlf true
+        git config --global core.longpaths true
+        git config --global --list
+        '''
   }
 
   if (env.BRANCH_NAME) {
